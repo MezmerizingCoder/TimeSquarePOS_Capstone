635dcd7408b0d253097919227ec2cd0c9badd926
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2022-09-01 17:01:43 +0800

    initial commit

--- src/pos_timesquare/model/User.java
+++ src/pos_timesquare/model/User.java
@@ -13,6 +13,16 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 
     public int getId() {
         return id;
