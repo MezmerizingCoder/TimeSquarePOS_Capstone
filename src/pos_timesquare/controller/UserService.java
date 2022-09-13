7a40cf7593d5993a999a0667146cf2df0ee240c1
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2022-09-13 17:40:39 +0800

    UserService auto generate database

--- src/pos_timesquare/controller/UserService.java
+++ src/pos_timesquare/controller/UserService.java
@@ -9,6 +9,8 @@ import java.sql.*;
 import java.util.List;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static pos_timesquare.controller.DatabaseConnection.getConnection;
 import pos_timesquare.model.User;
 
@@ -21,6 +23,56 @@ public class UserService {
     Connection conn = null;
     PreparedStatement pst;
     ResultSet rs;
+    
+    public UserService(){
+        Connection conn = getConnection();
+        try {
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet tables = dbmd.getTables(null, null, "User", null);
+            if (tables.next()) {
+                System.out.println("Exist");
+                
+                if(!dbmd.getColumns(null, null, "User", "username").next()){
+                    Statement stmt = conn.createStatement();
+                    String sql = "ALTER TABLE User ADD username TEXT"; 
+                    stmt.executeUpdate(sql);
+                }
+                if(!dbmd.getColumns(null, null, "User", "password").next()){
+                    Statement stmt = conn.createStatement();
+                    String sql = "ALTER TABLE User ADD password TEXT"; 
+                    stmt.executeUpdate(sql);
+                }
+                if(!dbmd.getColumns(null, null, "User", "name").next()){
+                    Statement stmt = conn.createStatement();
+                    String sql = "ALTER TABLE User ADD name TEXT"; 
+                    stmt.executeUpdate(sql);
+                }
+                if(!dbmd.getColumns(null, null, "User", "role").next()){
+                    Statement stmt = conn.createStatement();
+                    String sql = "ALTER TABLE User ADD role TEXT"; 
+                    stmt.executeUpdate(sql);
+                }
+                
+            }
+            else {
+                System.out.println("Not exist");
+                Statement stmt = conn.createStatement();
+                String sql = "CREATE TABLE User(" +
+                   "id INTEGER NOT NULL UNIQUE," +
+                   " username TEXT, " + 
+                   " password TEXT, " + 
+                   " name TEXT, " +
+                   " role TEXT, " +
+                   "PRIMARY KEY(id AUTOINCREMENT))"; 
+
+                stmt.executeUpdate(sql);
+                System.out.println("Created table in given database...");  
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
     
     public List<User> getAllUserDetails(){
