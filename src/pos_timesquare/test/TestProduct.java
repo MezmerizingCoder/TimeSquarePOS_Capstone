e6e3343a3fe14b19d588e0fe37ec1bc0088f29e6
CassaGuia <112837690+CassaGuia@users.noreply.github.com>
2022-09-20 22:13:29 +0800

    Product

---
+++ src/pos_timesquare/test/TestProduct.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos_timesquare.test;
+
+import java.util.List;
+import pos_timesquare.controller.ProductService;
+import pos_timesquare.model.Product;
+
+/**
+ *
+ * @author Cassandra
+ */
+public class TestProduct {
+    
+    public static void main(String[] args) {
+        ProductService user = new ProductService();
+        List<Product> list = user.getAllProductDetails();
+        
+        System.out.println(list);
+     
+      
+    }
+    
+}
